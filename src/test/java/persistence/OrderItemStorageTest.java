5e7af2190a129b7c07843907fa72f81130c4728a
Cemiroling <45895475+Cemiroling@users.noreply.github.com>
2021-04-06 12:59:24 +0300

    Added test

--- src/test/java/persistence/OrderItemStorageTest.java
+++ src/test/java/persistence/OrderItemStorageTest.java
@@ -1,8 +1,11 @@
 package persistence;
 
+import domain.Order;
 import domain.OrderItem;
+import org.hamcrest.number.OrderingComparison;
 import org.junit.jupiter.api.Test;
 
+import static data.TestSamples.getTestOrder;
 import static data.TestSamples.getValidOrderItem;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,14 +15,27 @@ class OrderItemStorageTest {
     private OrderItemStorage orderItemStorage = new OrderItemStorage();
 
     @Test
-    public void testPersistOrderItem() {
-        OrderItem order = getValidOrderItem();
+    public void persist() {
+        OrderItem orderItem = getValidOrderItem();
 
-        final String id = orderItemStorage.persist(order);
+        final String id = orderItemStorage.persist(orderItem);
 
         assertThat(id, is(notNullValue()));
 
         final OrderItem loadedByOrderId = orderItemStorage.loadByItemId(id);
-        assertThat(loadedByOrderId, is(equalTo(order)));
+        assertThat(loadedByOrderId, is(equalTo(orderItem)));
     }
+
+    @Test
+    public void persistOrderItems() {
+        Order order = getTestOrder();
+
+        final String[] id = orderItemStorage.persistOrderItems(order);
+
+        assertThat(id, is(notNullValue()));
+
+        final OrderItem[] loadedByOrderId = orderItemStorage.loadByOrderId(order.getId());
+        assertThat(loadedByOrderId, is(equalTo(order.getOrderItems())));
+    }
+
 }
